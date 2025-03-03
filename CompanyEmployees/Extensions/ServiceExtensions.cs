ecbbe40d3b9bc296dc3d7b933de5af8a50eeca57
Nguyễn Đình Thiên <nguyendinhthien031098@gmail.com>
2025-03-03 22:21:38 +0700

    Chap 7,8

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -49,5 +49,10 @@ namespace CompanyEmployees.Extensions
         {
             services.AddSqlServer<RepositoryContext>((configuration.GetConnectionString("sqlConnection")));
         }
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder)
+        {
+            return builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
+        }
     }
 }
--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -6,7 +6,7 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<Company, CompanyDto>().ForCtorParam("FullAddress", opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+        CreateMap<Company, CompanyDto>().ForMember(c => c.FullAddress, opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
         CreateMap<Employee, EmployeeDto>();
     }
 }
