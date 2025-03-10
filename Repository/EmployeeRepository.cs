3f6b7ff85048cab63cb18a199213ab2ff12d4a8e
Nguyễn Đình Thiên <nguyendinhthien031098@gmail.com>
2025-03-10 22:16:12 +0700

    chap 19

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -13,7 +13,7 @@ public class EmployeeRepository(RepositoryContext repositoryContext) : Repositor
         var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
                 .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
                 .Search(employeeParameters.SearchTerm)
-                .OrderBy(e => e.Name)
+                .Sort(employeeParameters.OrderBy)
                 .ToListAsync();
 
         return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Entities;
+using Repository.Extensions.Utility;
+using System.Linq.Dynamic.Core;
 
 namespace Repository.Extensions;
 
@@ -9,7 +11,7 @@ public static class RepositoryEmployeeExtensions
         return employees.Where(e => (e.Age >= minAge && e.Age <= maxAge));
     }
 
-    public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string searchTerm)
+    public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string? searchTerm)
     {
         if (string.IsNullOrWhiteSpace(searchTerm))
             return employees;
@@ -17,5 +19,18 @@ public static class RepositoryEmployeeExtensions
         var lowerCaseTerm = searchTerm.Trim().ToLower();
 
         return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+    }
+
+    public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string? orderByQueryString)
+    {
+        if (string.IsNullOrWhiteSpace(orderByQueryString))
+            return employees.OrderBy(e => e.Name);
+
+        var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+
+        if (string.IsNullOrWhiteSpace(orderQuery))
+            return employees.OrderBy(e => e.Name);
+
+        return employees.OrderBy(orderQuery);
     }
 }
